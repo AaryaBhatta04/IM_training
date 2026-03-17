79696330003171e757ab911e412698a159bb5224
Dev TEdge <developer@technizeredge.com>
2026-03-17 17:37:15 +0530

    Async Await Code

--- day2/1_ecma_demos/src/5_async/dom_handler.js
+++ day2/1_ecma_demos/src/5_async/dom_handler.js
@@ -1,3 +1,6 @@
+import postApiClient from "./post_api_client.js";
+// import getData from "./static_data.js";
+
 const messageDiv = document.querySelector('#messageDiv');
 const ajaxDiv = document.querySelector('#ajaxDiv');
 
@@ -13,6 +16,57 @@ button.innerHTML = "Load Data";
 const btnArea = document.querySelector('#aDiv_btnArea');
 btnArea.appendChild(button);
 
-button.addEventListener("click", () => {
-    alert("Button Click Executed...");
-})
+// button.addEventListener("click", () => {
+//     // alert("Button Click Executed...");
+//     const data = getData();
+//     generateRows(data);
+// });
+
+// button.addEventListener("click", () => {
+//     postApiClient.getAllPostsUsingCallback((data) => {
+//         generateRows(data);
+//     }, (eMsg) => {
+//         console.error(eMsg);
+//     });
+// });
+
+// button.addEventListener("click", () => {
+//     postApiClient.getAllPostsUsingPromise().then((data) => {
+//         generateRows(data);
+//     }).catch((eMsg) => {
+//         console.error(eMsg);
+//     });
+// });
+
+// button.addEventListener("click", async () => {
+//     try {
+//         const data = await postApiClient.getAllPostsUsingPromise();
+//         generateRows(data);
+//     } catch (eMsg) {
+//         console.error(eMsg);
+//     }
+// });
+
+button.addEventListener("click", async () => {
+    try {
+        const data = await postApiClient.getAllPostsAsync();
+        generateRows(data);
+    } catch (eMsg) {
+        console.error(eMsg);
+    }
+});
+
+function generateRows(data) {
+    const tbody = document.querySelector('#postTable tbody');
+    let row, cell;
+
+    for (const item of data) {
+        row = tbody.insertRow();
+        cell = row.insertCell();
+        cell.textContent = item.id;
+        cell = row.insertCell();
+        cell.textContent = item.title;
+        cell = row.insertCell();
+        cell.textContent = item.body;
+    }
+}
---
+++ day2/1_ecma_demos/src/5_async/post_api_client.js
@@ -0,0 +1,50 @@
+const url = "https://jsonplaceholder.typicode.com/posts";
+
+const postApiClient = {
+    // Not going to work
+    // getAllPosts: function() {
+    //     fetch(url).then(response=>{
+    //         response.json().then(data=>{
+    //             return (data);
+    //         }).catch(error=>{
+    //             console.error("Parsing Error")
+    //         })
+    //     }).catch(error=>{
+    //         console.error("Communication Error");
+    //     });
+    // }
+    getAllPostsUsingCallback: function (successCB, errorCB) {
+        fetch(url).then(response => {
+            response.json().then(data => {
+                successCB(data);
+            }).catch(error => {
+                errorCB("Parsing Error")
+            })
+        }).catch(error => {
+            errorCB("Communication Error");
+        });
+    },
+    getAllPostsUsingPromise: function () {
+        return new Promise((resolve, reject) => {
+            fetch(url).then(response => {
+                response.json().then(data => {
+                    resolve(data);
+                }).catch(error => {
+                    reject("Parsing Error")
+                })
+            }).catch(error => {
+                reject("Communication Error");
+            });
+        });
+    },
+    getAllPostsAsync: async function () {
+        try {
+            const response = await fetch(url);
+            return await response.json();
+        } catch(error) {
+            return new Error(error.message);
+        }
+    }
+};
+
+export default postApiClient;
