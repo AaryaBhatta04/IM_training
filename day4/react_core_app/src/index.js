5a212e5581b5d13af5a4bffcb5e94aa1e7a95617
Dev TEdge <developer@technizeredge.com>
2026-03-19 11:49:14 +0530

    Optimized Counter

--- day4/react_core_app/src/index.js
+++ day4/react_core_app/src/index.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
 import 'bootstrap';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import RootComponent from './components/root/RootComponent';
 
