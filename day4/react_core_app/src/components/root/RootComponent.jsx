518f947bbe418c96b2d5348c8d8bc55894f4473d
Dev TEdge <developer@technizeredge.com>
2026-03-19 15:30:14 +0530

    Reducer Code Refactored into files

--- day4/react_core_app/src/components/root/RootComponent.jsx
+++ day4/react_core_app/src/components/root/RootComponent.jsx
@@ -1,13 +1,15 @@
 // import CounterAssignment from "../1_assignment/CounterAssignment";
 // import CounterParentChildCom from "../2_parent_child_communication/CounterParentChildCom";
-import CounterWithReducer from "../3_reducers/CounterWithReducer";
+// import CounterWithReducer from "../3_reducers/CounterWithReducer";
+import CounterRoot from "../4_code_structuring/CounterRoot";
 
 const RootComponent = () => { 
     return (
         <div className="container mt-4">
             {/* <CounterAssignment /> */}
             {/* <CounterParentChildCom /> */}
-            <CounterWithReducer />
+            {/* <CounterWithReducer /> */}
+            <CounterRoot />
         </div>
     );
 }
