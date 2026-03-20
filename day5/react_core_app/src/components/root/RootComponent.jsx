2a70bd583e61bda11f0b5035a16dacb3e0bc7093
Dev TEdge <developer@technizeredge.com>
2026-03-20 10:44:54 +0530

    Calculator Assignment

--- day5/react_core_app/src/components/root/RootComponent.jsx
+++ day5/react_core_app/src/components/root/RootComponent.jsx
@@ -1,12 +1,13 @@
 // import EventComponent from "../1_react_events/EventComponent";
-
-import ControlledVsUncontrolledComponent from "../2_controlled_vs_uncontrolled/ControlledVsUncontrolledComponent";
+// import ControlledVsUncontrolledComponent from "../2_controlled_vs_uncontrolled/ControlledVsUncontrolledComponent";
+import CalculatorAssignment from "../3_assignment/CalculatorAssignment";
 
 const RootComponent = () => {
     return (
         <div className="container mt-4">
             {/* <EventComponent /> */}
-            <ControlledVsUncontrolledComponent />
+            {/* <ControlledVsUncontrolledComponent /> */}
+            <CalculatorAssignment />
         </div>
     );
 }
