5c3b317f83710ddfb8fca8d021bef73fa5ce01a0
Dev TEdge <developer@technizeredge.com>
2026-03-21 12:18:31 +0530

    Nested Routes

--- day6/react-routing/react_routing_app/src/components/products/ProductsComponent.jsx
+++ day6/react-routing/react_routing_app/src/components/products/ProductsComponent.jsx
@@ -1,9 +1,31 @@
+import { Link, Outlet } from "react-router-dom";
+import { useProducts } from "../../contexts/ProductsProvider";
+import './ProductsComponent.css';
 
 const ProductsComponent = () => {
+    const { products } = useProducts();
+
     return (
         <div className='text-center'>
             <h1 className="text-primary">Products Component</h1>
-            <h4 className="text-warning">This is a Simple, React Routing Application</h4>
+            <div className="row mt-5">
+                <div className="col">
+                    <div className="graybox">
+                        <ul className="list-group">
+                            {
+                                products.map((product) => (
+                                    <li key={product.id} className="list-group-item">
+                                        <Link to={`${product.id}`}>{product.name}</Link>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                </div>
+                <div className="col">
+                    <Outlet />
+                </div>
+            </div>
         </div>
     );
 };
--- day6/react-routing/react_routing_app/src/routes/index.js
+++ day6/react-routing/react_routing_app/src/routes/index.js
@@ -4,13 +4,23 @@ import AdminComponent from "../components/admin/AdminComponent";
 import HomeComponent from "../components/home/HomeComponent";
 import LoginComponent from "../components/login/LoginComponent";
 import NoMatchComponent from "../components/no_match/NoMatchComponent";
+import ProductDetailsComponent from "../components/products/ProductDetailsComponent";
+import ProductNotSelectedComponent from "../components/products/ProductNotSelectedComponent";
 import ProductsComponent from "../components/products/ProductsComponent";
+import ProductsProvider from "../contexts/ProductsProvider";
 
 export default (
     <Routes>
         <Route path="/" element={<HomeComponent />} />
         <Route path="/about" element={<AboutComponent />} />
-        <Route path="/products" element={<ProductsComponent />} />
+        <Route path="/products" element={
+            <ProductsProvider>
+                <ProductsComponent />
+            </ProductsProvider>
+        }>
+            <Route path="" element={<ProductNotSelectedComponent />} />
+            <Route path=":productId" element={<ProductDetailsComponent />} />
+        </Route>
         <Route path="/admin" element={<AdminComponent />} />
         <Route path="/login" element={<LoginComponent />} />
         <Route path="*" element={<NoMatchComponent />} />
