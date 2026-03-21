1f5d23f1b3a839790bd2678dc103d06c72a7c1ca
Dev TEdge <developer@technizeredge.com>
2026-03-21 15:44:31 +0530

    API call using fetch interceptor

--- day6/react-routing/react_routing_app/src/components/root/RootComponent.jsx
+++ day6/react-routing/react_routing_app/src/components/root/RootComponent.jsx
@@ -1,7 +1,48 @@
+import fetchIntercept from 'fetch-intercept';
+import { useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import authenticatorApiClient from '../../services/authenticator_api_client';
 import NavigationComponent from "../bs_nav/NavigationComponent";
 import ErrorBoundary from "../common/ErrorBoundary";
+
 const RootComponent = () => {
+    useEffect(() => {
+        const unregister = fetchIntercept.register({
+            request: function (url, config) {
+                config = config || {};
+                config.headers = config.headers || {};
+
+                config.headers['Accept'] = 'application/json';
+
+                if (url.includes('products')) {
+                    config.headers['x-access-token'] = authenticatorApiClient.getToken();
+                }
+
+                // Modify the url or config here
+                return [url, config];
+            },
+
+            requestError: function (error) {
+                // Called when an error occured during another 'request' interceptor call
+                return Promise.reject(error);
+            },
+
+            response: function (response) {
+                // Modify the reponse object
+                return response;
+            },
+
+            responseError: function (error) {
+                // Handle an fetch error
+                return Promise.reject(error);
+            }
+        });
+
+        return () => {
+            unregister();
+        };
+    }, []);
+
     return (
         <div className="container">
             <ErrorBoundary>
--- day6/react-routing/react_routing_app/src/services/products_api_client.js
+++ day6/react-routing/react_routing_app/src/services/products_api_client.js
@@ -1,18 +1,55 @@
-import authenticatorApiClient from "./authenticator_api_client";
+// import authenticatorApiClient from "./authenticator_api_client";
+
+// const url = process.env.REACT_APP_PRODUCTS_API_URL;
+
+// const productsAPIClient = {
+//     getAllProducts: function () {
+//         var promise = new Promise((resolve, reject) => {
+//             let fData = {
+//                 method: "GET",
+//                 headers: {
+//                     "x-access-token": authenticatorApiClient.getToken()
+//                 }
+//             };
+            
+//             fetch(url, fData).then((res) => {
+//                 if (!res.ok) {
+//                     const errorMessages = {
+//                         400: "Invalid request. Please try again.",
+//                         401: "Please log in to access this resource.",
+//                         403: "You do not have permission to access this resource.",
+//                         404: "The requested resource was not found.",
+//                         500: "Something went wrong on the server. Please try again later.",
+//                     };
+//                     reject(errorMessages[res.status] || `Unexpected error occurred (Code: ${res.status})`);
+//                     return;
+//                 }
+//                 var result = res.json();
+//                 result.then((jResult) => {
+//                     resolve(jResult);
+//                 }, (err) => {
+//                     reject("JSON Parse Error");
+//                 });
+//             }).catch((err) => {
+//                 console.log(err);
+//                 reject("Error connecting to the API");
+//             });
+//         });
+
+//         return promise;
+//     }
+// }
+
+// export default productsAPIClient;
+
+// ----------------------------------------------- Via Interceptor
 
 const url = process.env.REACT_APP_PRODUCTS_API_URL;
 
 const productsAPIClient = {
     getAllProducts: function () {
         var promise = new Promise((resolve, reject) => {
-            let fData = {
-                method: "GET",
-                headers: {
-                    "x-access-token": authenticatorApiClient.getToken()
-                }
-            };
-            
-            fetch(url, fData).then((res) => {
+            fetch(url).then((res) => {
                 if (!res.ok) {
                     const errorMessages = {
                         400: "Invalid request. Please try again.",
